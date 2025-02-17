1598f2b570caacf86c051feeb65b021469c9eeaf
Zeebuhh <ngroth277@gmail.com>
2025-02-17 11:46:13 +0100

    updated entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,7 @@ python manage.py migrate
 
 # Create a superuser if not exists
 if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ]; then
-    python manage.py createsuperuser --no-input --email "$DJANGO_SUPERUSER_EMAIL"
+    EMAIL=${DJANGO_SUPERUSER_EMAIL:-"admin@example.com"}
+    python manage.py createsuperuser --no-input --username "$DJANGO_SUPERUSER_USERNAME" --email "$EMAIL"
 fi
 exec "$@"
