cd21ad85035fa7033df8fdcbb8ffa2dfb34904fd
Zeebuhh <ngroth277@gmail.com>
2025-02-17 10:35:57 +0100

    added makemigrations

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
+
 # Apply database migrations
+python manage.py makemigrations
 python manage.py migrate
+
 # Create a superuser if not exists
 if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ]; then
     python manage.py createsuperuser --no-input
