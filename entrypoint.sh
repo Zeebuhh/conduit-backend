3848035199c0a4e67fc9f750563ff3139b622132
Zeebuhh <ngroth277@gmail.com>
2025-02-18 16:59:27 +0100

    submodule changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,13 +5,10 @@ set -e
 python manage.py makemigrations
 python manage.py migrate
 
-# Create a superuser if not exists
+# Create a superuser if not exists (using a one-liner)
 if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ]; then
     EMAIL=${DJANGO_SUPERUSER_EMAIL:-"admin@example.com"}
-    python manage.py shell -c "from django.contrib.auth import get_user_model; \
-User = get_user_model(); \
-if not User.objects.filter(username='$DJANGO_SUPERUSER_USERNAME').exists(): \
-    User.objects.create_superuser('$DJANGO_SUPERUSER_USERNAME', '$EMAIL', '$DJANGO_SUPERUSER_PASSWORD')"
+    python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); not User.objects.filter(username='$DJANGO_SUPERUSER_USERNAME').exists() and User.objects.create_superuser('$DJANGO_SUPERUSER_USERNAME', '$EMAIL', '$DJANGO_SUPERUSER_PASSWORD')"
 fi
 
 exec "$@"
